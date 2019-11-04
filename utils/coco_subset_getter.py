c1ff9fff3265ecdb9d30fc7b3d46cf1b655f99e3
Andrei-Sorin Popovici <pas.andrei@yahoo.com>
2019-11-04 07:31:13 +0200

    First working version of the coco dataset extractor

--- utils/coco_subset_getter.py
+++ utils/coco_subset_getter.py
@@ -14,7 +14,7 @@ def extract_from_annotations_file(annotations_file_path, folder, wanted_categori
         useful_images_to_labels = {}
         for annotation in data['annotations']:
             if annotation['category_id'] in wanted_categories_id:
-                image_id = annotation['id']
+                image_id = annotation['image_id']
                 if image_id not in useful_images_to_labels:
                     useful_images_to_labels[image_id] = []
                 useful_images_to_labels[image_id].append(annotation)
@@ -27,15 +27,15 @@ def extract_from_annotations_file(annotations_file_path, folder, wanted_categori
 
         print(len(image_id_to_image_file_name))
 
-        for image_id, annotation in useful_images_to_labels.items():
-            print(image_id)
-            print(image_id_to_image_file_name[image_id])
-            print(folder / image_id_to_image_file_name[image_id])
-            print(destination_images / image_id_to_image_info[image_id]['file_name'])
-            shutil.move(folder / image_id_to_image_file_name[image_id],
-                        destination_images / image_id_to_image_info[image_id]['file_name'])
+        for image_id, annotations in useful_images_to_labels.items():
+            # print(image_id)
+            # print(image_id_to_image_file_name[image_id])
+            # print(folder / image_id_to_image_file_name[image_id])
+            # print(destination_images / image_id_to_image_file_name[image_id])
+            shutil.copy(folder / image_id_to_image_file_name[image_id],
+                        destination_images / image_id_to_image_file_name[image_id])
             new_data['images'].append(image_id_to_image_info[image_id])
-            new_data['annotations'].append(useful_images_to_labels[image_id])
+            new_data['annotations'] = new_data['annotations'] + annotations
 
         for category in data['categories']:
             if category['id'] in wanted_categories_id:
@@ -49,18 +49,18 @@ def extract_from_annotations_file(annotations_file_path, folder, wanted_categori
 path = Path('C:\\Users\\Andrei Popovici\\Desktop\\COCO')
 destination = Path('C:\\Users\\Andrei Popovici\\Desktop\\COCO_new')
 
-wanted_categories_id = [1, 2]
+wanted_categories_id = [1, 3]
 
-train_annotations_path = path / 'annotations' / 'instances_train2017.json'
-train_folder_path = path / 'train2017'
-train_annotations_path_destionation = destination / 'annotations' / 'instances_train2017.json'
-train_folder_path_destination = destination / 'train2017'
-extract_from_annotations_file(train_annotations_path, train_folder_path, wanted_categories_id,
-                              train_annotations_path_destionation, train_folder_path_destination)
+# train_annotations_path = path / 'annotations' / 'instances_train2017.json'
+# train_folder_path = path / 'train2017'
+# train_annotations_path_destionation = destination / 'annotations' / 'instances_train2017.json'
+# train_folder_path_destination = destination / 'train2017'
+# extract_from_annotations_file(train_annotations_path, train_folder_path, wanted_categories_id,
+#                               train_folder_path_destination, train_annotations_path_destionation)
 
 val_annotations_path = path / 'annotations' / 'instances_val2017.json'
 val_folder_path = path / 'val2017'
-val_annotations_path_destionation = destination / 'annotations' / 'instances_train2017.json'
-val_folder_path_destination = destination / 'train2017'
+val_annotations_path_destionation = destination / 'annotations' / 'instances_val2017.json'
+val_folder_path_destination = destination / 'val2017'
 extract_from_annotations_file(val_annotations_path, val_folder_path, wanted_categories_id,
-                              val_annotations_path_destionation, val_folder_path_destination)
+                              val_folder_path_destination, val_annotations_path_destionation)
