222c4c8c974862d4ba5173772e447e8962a69116
Skiry <farauanuionut@gmail.com>
2019-11-18 17:43:02 +0200

    Add non-maximum suppression

--- data/dataset.py
+++ data/dataset.py
@@ -59,3 +59,47 @@ class CocoDetection(VisionDataset):
 
     def __len__(self):
         return len(self.ids)
+
+    def nms(self, boxes, overlap_threshold=0.3):
+        """
+        boxes: bouning boxes coordinates, ie, tuple of 4 integers
+        overlap threshold: the threshold for which the overlapping images will be suppressed
+        return the coordinates of the correct bounding boxes
+        """
+        if len(boxes) == 0:
+            return []
+
+        pick = []
+
+        x1 = boxes[:, 0]
+        y1 = boxes[:, 1]
+        x2 = boxes[:, 2]
+        y2 = boxes[:, 3]
+
+        area = (x2 - x1 + 1) * (y2 - y1 + 1)
+        idxs = np.argsort(y2)
+
+        while len(idxs) > 0:
+            last = len(idxs) - 1
+            i = idxs[last]
+            pick.append(i)
+            suppress = [last]
+
+            for pos in range(0, last):
+                j = idxs[pos]
+
+                xx1 = max(x1[i], x1[j])
+                yy1 = max(y1[i], y1[j])
+                xx2 = min(x2[i], x2[j])
+                yy2 = min(y2[i], y2[j])
+
+                w = max(0, xx2 - xx1 + 1)
+                h = max(0, yy2 - yy1 + 1)
+
+                overlap = float(w * h) / area[j]
+                if overlap > overlapThresh:
+                    suppress.append(pos)
+
+            idxs = np.delete(idxs, suppress)
+
+        return boxes[pick]
