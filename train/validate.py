6618b1c5c20b7eb62232faaada82bb5a9a86ddbe
Ronald <galearonald@yahoo.com>
2019-11-28 21:44:16 +0200

    basic tensorboard graphs

--- train/validate.py
+++ train/validate.py
@@ -3,19 +3,24 @@ import datetime
 
 from train.loss_fn import ssd_loss
 
+def update_tensorboard_graphs(writer, loc_loss_train, class_loss_train, loc_loss_val, class_loss_val, epoch):
+    writer.add_scalar('Localization Loss/train', loc_loss_train, epoch)
+    writer.add_scalar('Classification Loss/train', class_loss_train, epoch)
+    writer.add_scalar('Localization Loss/val', loc_loss_val, epoch)
+    writer.add_scalar('Classification Loss/val', class_loss_val, epoch)
 
-def evaluate(model, optimizer, anchors, grid_sizes, train_loader, valid_loader, losses, epoch, device, params):
+def evaluate(model, optimizer, anchors, grid_sizes, train_loader, valid_loader, losses, epoch, device, writer, params):
     '''
     evaluates model performance of the validation set, saves current set if it is better that the best so far
     '''
-
+    loc_loss_train, class_loss_train = losses[2] / len(train_loader.dataset), losses[3] / len(train_loader.dataset)
     print('Average loss this epoch: Localization: {}; Classification: {}'.format(
-        losses[2] / len(train_loader.dataset), losses[3] / len(train_loader.dataset)))
+        loc_loss_train, class_loss_train))
     print('Validation start...')
 
-    l_val_loss, c_val_loss = 0, 0
+    loc_loss_val, class_loss_val = 0, 0
 
-    BATCHES_TO_TEST = 4
+    BATCHES_TO_TEST = 1
     model.eval()
     with torch.no_grad():
         val_loss = 0
@@ -24,14 +29,15 @@ def evaluate(model, optimizer, anchors, grid_sizes, train_loader, valid_loader, 
             input_ = input_.to(device)
             output = model(input_)
             l_loss, c_loss = ssd_loss(output, label, anchors, grid_sizes, device, params)
-            l_val_loss += l_loss.item()
-            c_val_loss += c_loss.item()
+            loc_loss_val += l_loss.item()
+            class_loss_val += c_loss.item()
             if batch_idx == BATCHES_TO_TEST:
                 break
 
         # metric of performance... for now i take the loss
         SAVE_PATH = 'misc/experiments/{}/model_checkpoint'.format(params.model_id)
-        val_loss = (l_val_loss + c_val_loss) / BATCHES_TO_TEST
+        loc_loss_val, class_loss_val = loc_loss_val / BATCHES_TO_TEST, class_loss_val / BATCHES_TO_TEST
+        val_loss = loc_loss_val + class_loss_val
         if params.loss > val_loss:
             torch.save({
                 'epoch': epoch,
@@ -42,4 +48,9 @@ def evaluate(model, optimizer, anchors, grid_sizes, train_loader, valid_loader, 
             params.loss = val_loss
             params.save('misc/experiments/ssdnet/params.json')
             print('Model saved succesfully')
+
+
+        # tensorboard
+        update_tensorboard_graphs(writer, loc_loss_train, class_loss_train, loc_loss_val, class_loss_val, epoch)
+
     print('Validation finished')
