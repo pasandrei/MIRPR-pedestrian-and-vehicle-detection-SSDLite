5e00ec587d5fb1b7fb3fddd8f9656f88870bcaa3
Andrei-Sorin Popovici <pasandrei@users.noreply.github.com>
2019-11-30 18:18:53 +0200

    Remove images that contain too little information

--- utils/coco_subset_getter.py
+++ utils/coco_subset_getter.py
@@ -3,6 +3,19 @@ import shutil
 from pathlib import Path
 
 
+def compute_annotations_area_to_image_area(annotations, image_info):
+    annotations_area = 0
+    for annotation in annotations:
+        annotation_width = annotation['bbox'][2]
+        annotation_height = annotation['bbox'][3]
+
+        annotations_area += annotation_width * annotation_height
+
+    image_area = image_info['height'] * image_info['width']
+
+    return annotations_area / image_area
+
+
 def extract_from_annotations_file(annotations_file_path, folder, wanted_categories_id,
                                   destination_images, destination_annotation):
     print(annotations_file_path)
@@ -15,8 +28,10 @@ def extract_from_annotations_file(annotations_file_path, folder, wanted_categori
         for annotation in data['annotations']:
             if annotation['category_id'] in wanted_categories_id:
                 image_id = annotation['image_id']
+
                 if image_id not in useful_images_to_labels:
                     useful_images_to_labels[image_id] = []
+
                 useful_images_to_labels[image_id].append(annotation)
 
         image_id_to_image_file_name = {}
@@ -25,18 +40,20 @@ def extract_from_annotations_file(annotations_file_path, folder, wanted_categori
             image_id_to_image_file_name[image['id']] = image['file_name']
             image_id_to_image_info[image['id']] = image
 
-        print(len(image_id_to_image_file_name))
-
+        cnt = 0
         for image_id, annotations in useful_images_to_labels.items():
-            # print(image_id)
-            # print(image_id_to_image_file_name[image_id])
-            # print(folder / image_id_to_image_file_name[image_id])
-            # print(destination_images / image_id_to_image_file_name[image_id])
-            shutil.copy(folder / image_id_to_image_file_name[image_id],
-                        destination_images / image_id_to_image_file_name[image_id])
+            annotations_to_image_ratio = compute_annotations_area_to_image_area(annotations, image_id_to_image_info[image_id])
+
+            if annotations_to_image_ratio < 0.07:
+                cnt+=1
+                print(image_id_to_image_info[image_id], annotations_to_image_ratio)
+
+            # shutil.copy(folder / image_id_to_image_file_name[image_id],
+            #             destination_images / image_id_to_image_file_name[image_id])
             new_data['images'].append(image_id_to_image_info[image_id])
             new_data['annotations'] = new_data['annotations'] + annotations
 
+        print(cnt, len(useful_images_to_labels))
         for category in data['categories']:
             if category['id'] in wanted_categories_id:
                 new_data['categories'].append(category)
@@ -45,18 +62,17 @@ def extract_from_annotations_file(annotations_file_path, folder, wanted_categori
             json.dump(new_data, outfile)
 
 
-# path = sys.argv[1]
-path = Path('C:\\Users\\Andrei Popovici\\Desktop\\COCO')
-destination = Path('C:\\Users\\Andrei Popovici\\Desktop\\COCO_new')
+path = Path('D:\\COCO')
+destination = Path('D:\\COCO_new')
 
 wanted_categories_id = [1, 3]
 
-# train_annotations_path = path / 'annotations' / 'instances_train2017.json'
-# train_folder_path = path / 'train2017'
-# train_annotations_path_destionation = destination / 'annotations' / 'instances_train2017.json'
-# train_folder_path_destination = destination / 'train2017'
-# extract_from_annotations_file(train_annotations_path, train_folder_path, wanted_categories_id,
-#                               train_folder_path_destination, train_annotations_path_destionation)
+train_annotations_path = path / 'annotations' / 'instances_train2017.json'
+train_folder_path = path / 'train2017'
+train_annotations_path_destionation = destination / 'annotations' / 'instances_train2017.json'
+train_folder_path_destination = destination / 'train2017'
+extract_from_annotations_file(train_annotations_path, train_folder_path, wanted_categories_id,
+                              train_folder_path_destination, train_annotations_path_destionation)
 
 val_annotations_path = path / 'annotations' / 'instances_val2017.json'
 val_folder_path = path / 'val2017'
