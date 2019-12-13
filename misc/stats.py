dd9596adc8f0aee2b690332e4d7a40472c5ed3d3
Andrei-Sorin Popovici <pasandrei@users.noreply.github.com>
2019-12-13 16:36:12 +0200

    Statistics based on image resize to square shape (#47)
    It's perfect!

--- misc/stats.py
+++ misc/stats.py
@@ -5,21 +5,34 @@ import numpy as np
 from pathlib import Path
 
 
-def extract_from_annotations_file(annotations_file_path, folder, wanted_categories_id):
+def extract_from_annotations_file(annotations_file_path, wanted_categories_id):
     print(annotations_file_path)
-    print(folder)
 
     ratios = []
 
     with open(annotations_file_path, 'r') as annotations_file:
         data = json.load(annotations_file)
 
+        image_id_to_info = {}
+        for image in data['images']:
+            image_id = image['id']
+            image_id_to_info[image_id] = image
+
         for annotation in data['annotations']:
             if annotation['category_id'] in wanted_categories_id:
                 width = annotation['bbox'][2]
                 height = annotation['bbox'][3]
 
-                ratio = height/width
+                image_id = annotation['image_id']
+                image_info = image_id_to_info[image_id]
+
+                image_width = image_info['width']
+                image_height = image_info['height']
+
+                width /= image_width
+                height /= image_height
+
+                ratio = width/height
                 ln_ratio = math.log(ratio)
 
                 ratios.append(ln_ratio)
@@ -27,16 +40,23 @@ def extract_from_annotations_file(annotations_file_path, folder, wanted_categori
     ratios = np.array(ratios)
     plt.hist(ratios, bins=20)
     plt.axvline(ratios.mean(), color='k', linestyle='dashed', linewidth=1)
-    print(math.exp(ratios.mean()))
+    plt.axvline(ratios.mean()+ratios.std(), color='red', linestyle='dashed', linewidth=1)
+    plt.axvline(ratios.mean()-ratios.std(), color='red', linestyle='dashed', linewidth=1)
+    plt.axvline(ratios.mean()+2*ratios.std(), color='red', linestyle='dashed', linewidth=1)
+    plt.axvline(ratios.mean()-2*ratios.std(), color='red', linestyle='dashed', linewidth=1)
+    print("Mean: ", np.exp(ratios.mean()))
+    print("Mean + std: ", np.exp(ratios.mean() + ratios.std()))
+    print("Mean - std: ", np.exp(ratios.mean() - ratios.std()))
+    print("Mean + 2std: ", np.exp(ratios.mean() + 2*ratios.std()))
+    print("Mean - 2std: ", np.exp(ratios.mean() - 2*ratios.std()))
     plt.show()
 
 
 # path = sys.argv[1]
-path = Path('C:\\Users\\Andrei Popovici\\Desktop\\COCO_new')
+path = Path('C:\\Users\\Andrei Popovici\\Documents\\COCO')
 
-wanted_categories_id = [3]
+wanted_categories_id = [1]
 
 
-val_annotations_path = path / 'annotations' / 'instances_val2017.json'
-val_folder_path = path / 'val2017'
-extract_from_annotations_file(val_annotations_path, val_folder_path, wanted_categories_id)
+val_annotations_path = path / 'annotations' / 'instances_train2017.json'
+extract_from_annotations_file(val_annotations_path, wanted_categories_id)
