90a72293de2da9e6e35ef1516c73a88ce90a194f
Skiry <farauanuionut@gmail.com>
2019-12-16 14:59:48 +0200

    Add JAAD data getter
    Howto:
    Switch to JAAD-dataset branch
    Move the folder 'jaad_data' to a folder where you want the images
    Run the shell executable inside the 'jaad_data' folder

---
+++ jaad_data/vreau_jaad.sh
@@ -0,0 +1,9 @@
+#!/bash/bash
+git clone https://github.com/ykotseruba/JAAD 
+cp *.py JAAD
+curl http://data.nvision2.eecs.yorku.ca/JAAD_dataset/data/JAAD_clips.zip --output JAAD_clips.zip
+echo "Unzip the clips"
+echo "Now copy the *.py files with the ones from our github"
+
+
+
