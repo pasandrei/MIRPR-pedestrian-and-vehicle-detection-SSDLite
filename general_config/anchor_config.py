822b138c87eca6ff9f2b2091dad33322f711cf38
Andrei-Sorin Popovici <pasandrei@users.noreply.github.com>
2020-01-16 22:24:36 +0200

    Whole dataset training (#57)
    * whole dataset training prep
    
    * uncomment
    
    * Hotfixes
    
    * works on a dummy input
    
    to be tested on actual jaad images
    
    * fix typo, add inference call, uncomment
    
    * Add conversion from images into video
    
    * jad inference and resize to keep aspect ratio
    
    * we dont know
    
    * Add conversion from images into video
    
    * Removed unnecesary changes
    
    Co-authored-by: Raul Ronald Galea <41665873+RonaldGalea@users.noreply.github.com>
    Co-authored-by: Ionut Farauanu <farauanuionut@gmail.com>

--- general_config/anchor_config.py
+++ general_config/anchor_config.py
@@ -1,28 +1,34 @@
 from collections import OrderedDict
+from math import sqrt
 
 zoom = {
-    20: [],
-    10: [1., 1.2, 1.5, 1.8],
-    5: [0.75, 1., 1.25, 1.5],
-    3: [0.75, 1., 1.25, 1.5],
-    2: [0.75, 1., 1.25, 1.5],
-    1: [0.75, 1., 1.25, 1.5]
+    38: [1.],
+    19: [1.],
+    10: [1.],
+    5: [1.],
+    3: [1.],
+    1: [1.]
 }
 zoom = OrderedDict(zoom)
 
+sqrt2 = sqrt(2)
+sqrt3 = sqrt(3)
+
 ratio = {
-    20: [],
-    10: [(1., 1.), (1., 0.5), (0.5, 1)],
-    5: [(1., 1.), (1., 0.5), (0.5, 1), (2, 1), (1, 2)],
-    3: [(1., 1.), (1., 0.5), (0.5, 1), (2, 1), (1, 2)],
-    2: [(1., 1.), (1., 0.5), (0.5, 1), (2, 1), (1, 2)],
-    1: [(1., 1.), (1., 0.5), (0.5, 1), (2, 1), (1, 2)]
+    38: [(1., 1.), (1/sqrt2, 1*sqrt2), (1*sqrt2, 1/sqrt2), (1.3, 1.3)],
+    19: [(1., 1.), (1/sqrt2, 1*sqrt2), (1*sqrt2, 1/sqrt2), (1/sqrt3, 1*sqrt3), (1*sqrt3, 1/sqrt3), (1.3, 1.3)],
+    10: [(1., 1.), (1/sqrt2, 1*sqrt2), (1*sqrt2, 1/sqrt2), (1/sqrt3, 1*sqrt3), (1*sqrt3, 1/sqrt3), (1.3, 1.3)],
+    5: [(1., 1.), (1/sqrt2, 1*sqrt2), (1*sqrt2, 1/sqrt2), (1/sqrt3, 1*sqrt3), (1*sqrt3, 1/sqrt3), (1.3, 1.3)],
+    3: [(1., 1.), (1/sqrt2, 1*sqrt2), (1*sqrt2, 1/sqrt2), (1.3, 1.3)],
+    1: [(1., 1.), (1/sqrt2, 1*sqrt2), (1*sqrt2, 1/sqrt2), (1.3, 1.3)]
 }
 ratio = OrderedDict(ratio)
 
 # anchors per feature map cell for each grid
 k_list = [len(v_zoom) * len(v_ratio) for (_, v_zoom), (_, v_ratio)
-         in zip(zoom.items(), ratio.items())]
+          in zip(zoom.items(), ratio.items())]
 
 total_anchors = sum([len(v_zoom) * len(v_ratio) * k_zoom**2 for (k_zoom, v_zoom), (_, v_ratio)
-         in zip(zoom.items(), ratio.items())])
+                     in zip(zoom.items(), ratio.items())])
+
+print("total_anchors: ", total_anchors)
--- jaad_data/inference.py
+++ jaad_data/inference.py
@@ -1,24 +1,83 @@
-import os, cv2
-rootdir = '.'
+import torchvision.transforms.functional as F
+from PIL import Image
+import os
+import cv2
+import copy
+import numpy as np
+import torch
 
-for subdir, _, files in os.walk(rootdir):
-    new_dir_name = subdir + "_inference"
-    try:
-        subdir.index("video") 
-        v = 0
+from misc import postprocessing
+from misc.utils import *
+
+rootdir = 'C:\\Users\Andrei Popovici\Desktop\JAAD_stuff\JAAD-JAAD_2.0\images'
+
+
+def jaad_inference(model, output_handler):
+
+    for subdir, _, files in os.walk(rootdir):
+        new_dir_name = subdir + "_inference"
         try:
-            subdir.index("inference")
-        except Exception as ex:
-            v = 1
-        if not v:
-            raise Exception()
-        os.mkdir(new_dir_name)
-    except Exception as e:
-        print(new_dir_name + " already exists or does not contain images")
-        continue
-    for file in files:
-        image_path = os.path.join(subdir, file)
-        img = cv2.imread(image_path) # image as numpy array
-        #inference
-        inference_image_path = os.path.join(new_dir_name, file)
-        cv2.imwrite(inference_image_path, img)
+            subdir.index("video")
+            v = 0
+            try:
+                subdir.index("inference")
+            except Exception as ex:
+                v = 1
+            if not v:
+                raise Exception()
+            os.mkdir(new_dir_name)
+        except Exception as e:
+            print(new_dir_name + " already exists or does not contain images")
+            continue
+        for i, file in enumerate(files):
+            image_path = os.path.join(subdir, file)
+            img = cv2.imread(image_path)  # image as numpy array
+            img = feed_to_model(model, img, output_handler)
+            inference_image_path = os.path.join(new_dir_name, file)
+            cv2.imwrite(inference_image_path, img)
+            # if i == 20:
+            #     break
+        break
+
+
+def crop_center(img, cropx, cropy):
+    y, x = img.shape[0], img.shape[1]
+    startx = x//2-(cropx//2)
+    starty = y//2-(cropy//2)
+    return img[starty:starty+cropy, startx:startx+cropx]
+
+
+def feed_to_model(model, img, output_handler):
+    with torch.no_grad():
+        model.eval()
+        init_size = img.shape
+
+        img = crop_center(img, 850, 850)
+        img1 = copy.deepcopy(img)
+
+        init_size = [850, 850]
+        img = Image.fromarray(img.astype('uint8'), 'RGB')
+        img = F.resize(img, size=(320, 320), interpolation=2)
+        img = F.to_tensor(img)
+        img = F.normalize(img, mean=[0.53462092, 0.52424837, 0.53687582],
+                          std=[0.19282397, 0.18090153, 0.18164604])
+        # add batch channel
+        img = img.view((1, img.shape[0], img.shape[1], img.shape[2]))
+
+        img = img.to(torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
+        predictions = model(img)
+
+        prediction_bboxes, predicted_classes, _, _ = output_handler._get_sorted_predictions(
+            predictions[0][0], predictions[1][0], (0, (init_size[0], init_size[1])))
+
+        indeces_kept_by_nms = postprocessing.nms(prediction_bboxes, predicted_classes,
+                                                 output_handler.suppress_threshold)
+
+        final_bbox = prediction_bboxes[indeces_kept_by_nms]
+        final_class = predicted_classes[indeces_kept_by_nms]
+
+        img = img.view((img.shape[1], img.shape[2], img.shape[3]))
+        img = output_handler._unnorm_scale_image(img)
+
+        return plot_bounding_boxes(image=img1, bounding_boxes=final_bbox, classes=final_class,
+                                   bbox_type="pred", size=(init_size[0], init_size[1]))
