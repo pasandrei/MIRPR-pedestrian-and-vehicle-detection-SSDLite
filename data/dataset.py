d87a10d0fa91c885489477798c369f0ceb4d636b
Andrei-Sorin Popovici <pasandrei@users.noreply.github.com>
2020-01-22 00:26:44 +0200

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -50,6 +50,9 @@ class CocoDetection(VisionDataset):
             # target[0] = tensor of bboxes of objects in image
             # target[1] = tensor of class ids in image
             target = prepare_gt(img, target)
+            
+            if len(target[0].shape) < 2:
+                continue
 
             width, height = img.size
 
@@ -62,8 +65,6 @@ class CocoDetection(VisionDataset):
             img = F.normalize(img, mean=[0.485, 0.456, 0.406],
                               std=[0.229, 0.224, 0.225])
 
-            if len(target[0].shape) < 2:
-                continue
             imgs.append(img)
             targets_bboxes.append(target[0])
             targets_classes.append(target[1])
