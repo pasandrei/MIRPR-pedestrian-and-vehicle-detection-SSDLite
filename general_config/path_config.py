021b636b936781b94ade64831796c95682754ac7
RonaldGalea <41665873+RonaldGalea@users.noreply.github.com>
2020-02-14 21:07:53 +0200

    add paths

--- general_config/path_config.py
+++ general_config/path_config.py
@@ -9,5 +9,6 @@ val_annotations_path = dataset_root / 'annotations/instances_val2017.json'
 val_images_folder = dataset_root / 'val2017'
 
 params_path = 'misc/experiments/{}/params.json'
-model_save_path = 'misc/experiments/{}/model_checkpoint'
-model_save_path_loss = 'misc/experiments/{}/model_checkpoint_loss'
+stats_path = 'misc/experiments/{}/stats.json'
+model_path = 'misc/experiments/{}/model_checkpoint'
+model_path_loss = 'misc/experiments/{}/model_checkpoint_loss'
