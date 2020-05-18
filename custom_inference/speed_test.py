7154f70d20185aee289bebd2ea263ae0fa7b5c07
RonaldGalea <raulronaldgalea@yahoo.com>
2020-05-18 17:06:04 +0300

    add averaging to speed test

--- custom_inference/speed_test.py
+++ custom_inference/speed_test.py
@@ -11,8 +11,9 @@ from data import dataloaders
 
 
 class Speed_testing():
-    def __init__(self, runs=100, print_each_run=False):
+    def __init__(self, runs=10, n_images=100, print_each_run=False):
         self.runs = runs
+        self.n_images = n_images
         self.device = general_config.device
         self.params = Params(constants.params_path.format(general_config.model_id))
 
@@ -33,7 +34,6 @@ class Speed_testing():
         device - cuda:0 or cpu
         !!! overwrites the original settings
         """
-        self.valid_loader_iter = iter(self.valid_loader)
         if custom_settings:
             print("Current custom settings: ", custom_settings)
             nms_thresh, conf_thresh, device = custom_settings
@@ -41,51 +41,47 @@ class Speed_testing():
             self.output_handler.confidence_threshold = conf_thresh
             self.device = device
             self.model.to(self.device)
+
         run = 0
-        times_model, times_pre_nms, times_nms, in_nms_boxes = [], [], [], 0
+        total_time_model, total_time_pre_nms, total_time_nms, in_nms_boxes = 0, 0, 0, 0
         while run < self.runs:
-            (boxes, confs), image_info, last_model = self.val_image_output()
-            times_model.append(last_model)
+            c_img = 0
+            times_model, times_pre_nms, times_nms, in_nms_boxes = 0, 0, 0, 0
+            self.valid_loader_iter = iter(self.valid_loader)
+            while c_img < self.n_images:
+                (boxes, confs), image_info, last_model = self.val_image_output()
+                times_model += last_model
 
-            start_pre_nms = time.time()
-            boxes, classes = postprocess_until_nms(self.output_handler, boxes,
-                                                   confs, image_info[0][1])
-            last_pre_nms = time.time() - start_pre_nms
-            times_pre_nms.append(last_pre_nms)
+                start_pre_nms = time.time()
+                boxes, classes = postprocess_until_nms(self.output_handler, boxes,
+                                                       confs, image_info[0][1])
+                last_pre_nms = time.time() - start_pre_nms
+                times_pre_nms += last_pre_nms
 
-            boxes = wh2corners_numpy(boxes[:, :2], boxes[:, 2:])
-            start_nms = time.time()
-            boxes = boxes[:200]
-            in_nms_boxes += len(boxes)
-            _ = nms(boxes, classes, self.output_handler.suppress_threshold)
-            last_nms = time.time() - start_nms
-            times_nms.append(last_nms)
+                boxes = wh2corners_numpy(boxes[:, :2], boxes[:, 2:])
+                start_nms = time.time()
+                boxes = boxes[:200]
+                in_nms_boxes += len(boxes)
+                _ = nms(boxes, classes, self.output_handler.suppress_threshold)
+                last_nms = time.time() - start_nms
+                times_nms += last_nms
+                c_img += 1
+
             if self.print_each_run:
-                print("Time for last run\n")
-                print("Model: ", last_model)
-                print("Pre nms: ", last_pre_nms)
-                print("NMS: ", last_nms, "\n")
+                self.print_stats(times_model, times_pre_nms, times_nms, self.n_images)
+                print("Mean number of boxes processed by nms: ",
+                      "{:.2f}".format(in_nms_boxes / self.n_images))
             run += 1
 
-        print("Done ", self.runs, " runs")
-        print("\n\n\n")
+            total_time_model += times_model
+            total_time_pre_nms += times_pre_nms
+            total_time_nms += times_nms
 
-        total_model, total_pre_nms, total_nms = sum(times_model), sum(times_pre_nms), sum(times_nms)
-        print("Total time of model: ", "{:.2f}".format(total_model))
-        print("Mean time model: ", "{:.2f}".format(total_model / len(times_model)))
-        print("Total time of pre nms: ", "{:.2f}".format(total_pre_nms))
-        print("Mean time pre nms: ", "{:.2f}".format(total_pre_nms / len(times_pre_nms)))
-        print("Total time of nms: ", "{:.2f}".format(total_nms))
-        print("Mean time nms: ", "{:.2f}".format(total_nms / len(times_nms)))
-        print("Mean number of boxes processed by nms: ", "{:.2f}".format(in_nms_boxes / len(times_nms)))
-
-        print("\n\n\n")
-
-        total_time = sum(times_model + times_pre_nms + times_nms)
-        print("Total time taken: ", total_time)
-        print("Percentage of model: ", "{:.2f}".format(total_model / total_time * 100))
-        print("Percentage of pre nms: ", "{:.2f}".format(total_pre_nms / total_time * 100))
-        print("Percentage of nms: ", "{:.2f}".format(total_nms / total_time * 100))
+        print("Final results:")
+        print("--------------------------------------")
+        print("--------------------------------------\n\n")
+        self.print_stats(total_time_model, total_time_pre_nms, total_time_nms,
+                         self.n_images * self.runs)
 
     def val_image_output(self):
         with torch.no_grad():
@@ -97,3 +93,21 @@ class Speed_testing():
             confs = confs.squeeze().permute(1, 0)
             last_model = time.time() - start
             return (boxes, confs), image_info, last_model
+
+    def print_stats(self, total_model, total_pre_nms, total_nms, avg_factor):
+        print("Total time of model: ", "{:.4f}".format(total_model))
+        print("Mean time model: ", "{:.4f}".format(total_model / avg_factor))
+        print("Total time of pre nms: ", "{:.4f}".format(total_pre_nms))
+        print("Mean time pre nms: ", "{:.4f}".format(total_pre_nms / avg_factor))
+        print("Total time of nms: ", "{:.4f}".format(total_nms))
+        print("Mean time nms: ", "{:.4f}".format(total_nms / avg_factor))
+
+        print("\n\n\n")
+
+        total_time = total_model + total_pre_nms + total_nms
+        print("Total time taken: ", total_time)
+        print("Percentage of model: ", "{:.4f}".format(total_model / total_time * 100))
+        print("Percentage of pre nms: ", "{:.4f}".format(total_pre_nms / total_time * 100))
+        print("Percentage of nms: ", "{:.4f}".format(total_nms / total_time * 100))
+
+        print("--------------------------------------\n\n")
