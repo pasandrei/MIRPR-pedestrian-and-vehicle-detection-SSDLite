75c1371f0361fbfc00e7ce0e5b2e3a3a809d1d4e
Andrei Popovici <pasandrei98@gmail.com>
2026-03-14 00:25:32 +0200

    Change warmup start from LR/100 to LR/200

--- train/lr_policies/base_lr_handler.py
+++ train/lr_policies/base_lr_handler.py
@@ -10,10 +10,10 @@ class BaseLrHandler():
 
     def warm_up(self, epoch, batch_idx, train_size, warmup_epochs=1):
         """
-        linearly increase learning_rate from lr/100 to lr over warmup_epochs
+        linearly increase learning_rate from lr/200 to lr over warmup_epochs
         """
         batch_idx += 1
         progress = (epoch * train_size + batch_idx) / (warmup_epochs * train_size)
         for i, param_group in enumerate(self.optimizer.param_groups):
-            lr = self.param_group_lrs[i] / 100 + progress * self.param_group_lrs[i] * 0.99
+            lr = self.param_group_lrs[i] / 200 + progress * self.param_group_lrs[i] * (199/200)
             param_group['lr'] = lr
